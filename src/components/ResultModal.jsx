6986d5f00041ec48902fe73668d2f93749d5af62
Saint1225 <vincent-valentine1225@hotmail.com>
2024-01-16 22:39:36 +0800

    Move ResultModal component to outter layer with createPortal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(
   ({ targetTime, remainingTime, handleReset }, ref) => {
@@ -12,10 +13,12 @@ const ResultModal = forwardRef(
 
     const userLost = remainingTime <= 0;
     const timeLeft = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-    return (
-      <dialog ref={dialogRef} className="result-modal">
+    return createPortal(
+      <dialog ref={dialogRef} className="result-modal" onClose={handleReset}>
         <h2>You {userLost ? "lost" : "won"}</h2>
+        {!userLost && <h2>Your Score: {score}</h2>}
         <p>
           The target time was <strong>{targetTime} seconds.</strong>
         </p>
@@ -25,7 +28,8 @@ const ResultModal = forwardRef(
         <form method="dialog" onSubmit={handleReset}>
           <button>Close</button>
         </form>
-      </dialog>
+      </dialog>,
+      document.getElementById("modal")
     );
   }
 );
